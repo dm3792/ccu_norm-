d407352d40654673f86114e5769e9393bbc54fa2
dm3792 <122396971+dm3792@users.noreply.github.com>
2023-04-26 22:21:46 -0400

    Update ldcNormsCombine.py

--- ldcNormsCombine.py
+++ ldcNormsCombine.py
@@ -31,7 +31,8 @@ def generate_input(split,utt_before,utt_after):
     }
     for item in content:
         
-        
+        print(item)
+        print(content[item])
         if(content[item]['split']!=split):
             continue
 
