182e26af277c17d0d7e42f82e71345d75ba32b8e
Dhaarna Maheshwari <dhaarna@Dhaarnas-MacBook-Air.local>
2023-05-04 07:38:07 -0400

    ldc

--- ldcNormsCombine.py
+++ ldcNormsCombine.py
@@ -30,9 +30,10 @@ def generate_input(split,utt_before,utt_after):
     118 :'respect',
     119 :'flattery'
     }
+    done=0
     for item in content:
         
-        done=0
+        
         if(split in content[item]['splits']) or done==1:
             continue
 
