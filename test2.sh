cef66d8d1f37fcf88ad9118afa49d0858ae8af32
Dhaarna Maheshwari <dhaarna@Dhaarnas-MacBook-Air.local>
2023-05-10 14:44:32 -0400

    sh

--- test2.sh
+++ test2.sh
@@ -1,5 +1,5 @@
-"python3 modelccu.py --device=cuda --batch-size=4 --regularisation=dropout --include-utterance"
-"python3 modelccu.py --device=cuda --batch-size=4 --downsample=4 --include-utterance"
-"python3 modelccu.py --device=cuda --batch-size=4 --lrscheduler --include-utterance"
-"python3 modelccu.py --device=cuda --batch-size=4 --classifierlayers=2 --include-utterance"
-"python3 modelccu.py --device=cuda --batch-size=4 --condfident-only --include-utterance"
+python3 modelccu.py --device=cuda --batch-size=4 --regularisation=dropout --include-utterance
+python3 modelccu.py --device=cuda --batch-size=4 --downsample=4 --include-utterance
+python3 modelccu.py --device=cuda --batch-size=4 --lrscheduler --include-utterance
+python3 modelccu.py --device=cuda --batch-size=4 --classifierlayers=2 --include-utterance
+python3 modelccu.py --device=cuda --batch-size=4 --condfident-only --include-utterance
