95f1d221b1c508081c5defc9e0cfd5feb6eecdb2
Dhaarna Maheshwari <dhaarna@Dhaarnas-MacBook-Air.local>
2023-05-12 09:21:18 -0500

    sh

--- check.py
+++ check.py
@@ -1,11 +1,11 @@
 import pickle
  
-with open('train_1.pickle', 'rb') as pickle_file:
+with open('train_2.pickle', 'rb') as pickle_file:
     content = pickle.load(pickle_file)
     print(len(content))
-with open('val_1.pickle', 'rb') as pickle_file:
+with open('val_2.pickle', 'rb') as pickle_file:
     content = pickle.load(pickle_file)
     print(len(content))
-with open('test_1.pickle', 'rb') as pickle_file:
+with open('test_2.pickle', 'rb') as pickle_file:
     content = pickle.load(pickle_file)
     print(len(content))
